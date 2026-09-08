0b8cb3ba68869df967e3c717f5ed9cba78e2032f
ttwuddj-lang <ttwuddj@gmail.com>
2026-09-08 11:58:48 +0530

    Add files via upload
    Signed-off-by: ttwuddj-lang <ttwuddj@gmail.com>

---
+++ SPOTIFY_MUSIC/core/bot-1.py
@@ -0,0 +1,71 @@
+
+# ======================================================================
+# ||                                                               ||
+# ||   ██████╗  █████╗ ██████╗ ██╗   ██╗███████╗███████╗██╗ ██████╗  ||
+# ||   ██╔══██╗██╔══██╗██╔══██╗██║   ██║██╔════╝██╔════╝██║██╔═══██╗ ||
+# ||   ██████╔╝███████║██████╔╝██║   ██║█████╗  ███████╗██║██║   ██║ ||
+# ||   ██╔══██╗██╔══██║██╔══██╗██║   ██║██╔══╝  ╚════██║██║██║▄▄ ██║ ||
+# ||   ██████╔╝██║  ██║██████╔╝╚██████╔╝███████╗███████║██║╚██████╔╝ ||
+# ||   ╚═════╝ ╚═╝  ╚═╝╚═════╝  ╚═════╝ ╚══════╝╚══════╝╚═╝ ╚══▀▀═╝  ||
+# ║    ▓▒░ ʙ ᴀ ʙ ɪ ᴇ sＩＱ ░▒▓  s ᴇ ᴄ ᴜ ʀ ᴇ  ▓▒░ ɴ ᴇ ᴛ ᴡ ᴏ ʀ ᴋ ░▒▓    ║
+# ||                                                               ||
+# ======================================================================
+# || PROJECT  : SPOTIFY_MUSIC Public Music Repository                  ||
+# || AUTHOR   : BabiesIQ Team                                      ||
+# || REPO     : github.com/BABY-MUSIC/SPOTIFY_MUSIC                ||
+# || API      : www.babyapi.pro                                    ||
+# || TELEGRAM : t.me/BabiesIQ                                      ||
+# ----------------------------------------------------------------------
+# || LEGAL NOTICE                                                  ||
+# || Use / upload / modify at your own risk.                       ||
+# || Only config /.env edit allowed.                               ||
+# || Do not modify core files.                                     ||
+# || Keep this header if forked.                                   ||
+# || Dev not responsible for ban / damage / api block.             ||
+# ----------------------------------------------------------------------
+# || SECURITY                                                      ||
+# || Internal protection may exist.                                ||
+# || Unauthorized change may stop system.                          ||
+# || Use official API only -> www.babyapi.pro                      ||
+# ======================================================================
+
+
+from pyrogram import Client, errors
+from pyrogram.enums import ChatMemberStatus, ParseMode
+
+import config
+
+from ..logging import LOGGER
+
+
+class BABY(Client):
+    def __init__(self):
+        LOGGER(__name__).info(f"Starting Bot...")
+        super().__init__(
+            name="SPOTIFY_MUSIC",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            bot_token=config.BOT_TOKEN,
+            in_memory=True,
+            max_concurrent_transmissions=7,
+        )
+
+    async def start(self):
+        await super().start()
+        self.id = self.me.id
+        self.name = self.me.first_name + " " + (self.me.last_name or "")
+        self.username = self.me.username
+        self.mention = self.me.mention
+
+        try:
+            await self.send_message(
+                chat_id=config.LOGGER_ID,
+                text=f"<u><b>» {self.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :</b></u>\n\n"
+                     f"ɪᴅ : <code>{self.id}</code>\n"
+                     f"ɴᴀᴍᴇ : {self.name}\n"
+                     f"ᴜsᴇʀɴᴀᴍᴇ : @{self.username}",
+            )
+        except Exception as ex:
+            LOGGER(__name__).error(
+                f"LOGGER ERROR: {type(ex).__name__}: {ex}"
+            )
